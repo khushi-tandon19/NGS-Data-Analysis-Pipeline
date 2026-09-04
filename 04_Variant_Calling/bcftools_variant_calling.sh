452af6bf81de32a8e2860fc73cef734f7fe65b7f
khushi-tandon19 <pankhuritandon16@gmail.com>
2026-09-04 19:51:09 +0530

    Update bcftools_variant_calling.sh

--- 04_Variant_Calling/bcftools_variant_calling.sh
+++ 04_Variant_Calling/bcftools_variant_calling.sh
@@ -8,11 +8,11 @@ mkdir -p ../results/variants
 # Generate pileup and call variants
 bcftools mpileup -f "$REFERENCE" "$BAM" -Ou | \
 bcftools call -mv -Oz \
--o ../results/variants/sample.vcf.gz
+    -o ../results/variants/sample.vcf.gz
 
 # Index VCF
 bcftools index ../results/variants/sample.vcf.gz
 
 # Create readable VCF
 bcftools view ../results/variants/sample.vcf.gz \
-> ../results/variants/sample_variants.vcf
+    > ../results/variants/sample_variants.vcf
